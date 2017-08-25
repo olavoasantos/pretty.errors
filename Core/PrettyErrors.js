69acf942a621c19d921485aacac9e05005be981c
Olavo <olavo.a.santos@gmail.com>
2017-08-25 18:45:30 -0300

    Revert "README and bug fixes"
    This reverts commit d0db67faa729978c7a15ba8a67946012c2efee8a.

--- Core/PrettyErrors.js
+++ Core/PrettyErrors.js
@@ -1,22 +1,7 @@
 let ErrorParser = require('./ErrorParser');
 let ErrorPresentor = require('./ErrorPresentor');
 
-class PrettyErrors extends Error {
-
-    constructor(message, name, code) {
-        super(message);
-
-        if(name) this.name = name;
-        if(code) this.code = code;
-
-        this.toString();
-        process.exit();
-    }
-
-    toString() {
-        let prettyError = (new ErrorParser).parse(this);
-        (new ErrorPresentor).print(prettyError);
-    }
+class PrettyErrors {
 
     static throw(error) {
         let prettyError = (new ErrorParser).parse(error);

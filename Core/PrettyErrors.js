d0db67faa729978c7a15ba8a67946012c2efee8a
Olavo <olavo.a.santos@gmail.com>
2017-08-25 18:40:32 -0300

    README and bug fixes

--- Core/PrettyErrors.js
+++ Core/PrettyErrors.js
@@ -1,7 +1,22 @@
 let ErrorParser = require('./ErrorParser');
 let ErrorPresentor = require('./ErrorPresentor');
 
-class PrettyErrors {
+class PrettyErrors extends Error {
+
+    constructor(message, name, code) {
+        super(message);
+
+        if(name) this.name = name;
+        if(code) this.code = code;
+
+        this.toString();
+        process.exit();
+    }
+
+    toString() {
+        let prettyError = (new ErrorParser).parse(this);
+        (new ErrorPresentor).print(prettyError);
+    }
 
     static throw(error) {
         let prettyError = (new ErrorParser).parse(error);
